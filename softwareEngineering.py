e464c110f1c2ac762596278ed7bcd468066d400b
reva <reva.adi@ufl.edu>
2023-03-07 23:03:15 -0500

    added decoder function

--- softwareEngineering.py
+++ softwareEngineering.py
@@ -7,6 +7,14 @@ def encode(intString):
         newString += str((int(i) + 3) % 10)
     return newString
 
+# This is Reva Adi's decoder function
+def decode(password):
+    res = ""
+    for num in range(len(password)):
+        value = int(password[num])
+        new_value = (value + 3) % 10
+        res += str(new_value)
+    return res
 
 def main():
     newString = ""
@@ -20,14 +28,27 @@ def main():
         print()
         print("Please enter an option: ", end="")
         chosenInt = int(input())
+        if chosenInt == 1:
+            print("Please enter your password to encode: ", end="")
+            password = input()
+            newString = encode(password)
+            print("Your password has been encoded and stored!")
+            print()
+        if chosenInt == 2:
+            old_password = decode(newString)
+            print("The encoded password is " + newString, end="")
+            print(", and the original password is " + old_password + ".")
+            print()
+        if chosenInt == 3:
+            quit()
+
+
+
+main()
+
+'''
         if chosenInt < 3:
             # takes user input for password
             print("Please enter your password to encode: ", end="")
             password = input()
-            if chosenInt == 1:
-                newString = encode(password)
-                print("Your password has been encoded and stored!")
-                print()
-
-
-main()
+'''
